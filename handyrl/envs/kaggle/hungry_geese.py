007e3c7be02fafb2cbe0e379d9fe96957df505a7
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-03-12 09:17:00 +0900

    use custom model and reset training

--- handyrl/envs/kaggle/hungry_geese.py
+++ handyrl/envs/kaggle/hungry_geese.py
@@ -30,8 +30,8 @@ class TorusConv2d(nn.Module):
         self.bn = nn.BatchNorm2d(output_dim) if bn else None
 
     def forward(self, x):
-        h = torch.cat([x[:,:,:,-self.edge_size[1]:], x, x[:,:,:,:self.edge_size[1]]], dim=3)
-        h = torch.cat([h[:,:,-self.edge_size[0]:], h, h[:,:,:self.edge_size[0]]], dim=2)
+        h = torch.cat([x[:, :, :, -self.edge_size[1] :], x, x[:, :, :, : self.edge_size[1]]], dim=3)
+        h = torch.cat([h[:, :, -self.edge_size[0] :], h, h[:, :, : self.edge_size[0]]], dim=2)
         h = self.conv(h)
         h = self.bn(h) if self.bn is not None else h
         return h
@@ -44,19 +44,31 @@ class GeeseNet(BaseModel):
         layers, filters = 12, 32
         self.conv0 = TorusConv2d(input_shape[0], filters, (3, 3), True)
         self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+
+        self.conv_p = TorusConv2d(filters, filters, (3, 3), True)
+        self.conv_v = TorusConv2d(filters, filters, (3, 3), True)
+
         self.head_p = nn.Linear(filters, 4, bias=False)
-        self.head_v = nn.Linear(filters * 2, 1, bias=False)
+        self.head_v1 = nn.Linear(filters * 2, filters, bias=False)
+        self.head_v2 = nn.Linear(filters, 1, bias=False)
 
     def forward(self, x, _=None):
         h = F.relu_(self.conv0(x))
         for block in self.blocks:
             h = F.relu_(h + block(h))
-        h_head = (h * x[:,:1]).view(h.size(0), h.size(1), -1).sum(-1)
-        h_avg = h.view(h.size(0), h.size(1), -1).mean(-1)
-        p = self.head_p(h_head)
-        v = torch.tanh(self.head_v(torch.cat([h_head, h_avg], 1)))
 
-        return {'policy': p, 'value': v}
+        h_p = F.relu_(self.conv_p(h))
+        h_head_p = (h_p * x[:, :1]).view(h_p.size(0), h_p.size(1), -1).sum(-1)
+        p = self.head_p(h_head_p)
+
+        h_v = F.relu_(self.conv_v(h))
+        h_head_v = (h_v * x[:, :1]).view(h_v.size(0), h_v.size(1), -1).sum(-1)
+        h_avg_v = h_v.view(h_v.size(0), h_v.size(1), -1).mean(-1)
+
+        h_v = F.relu_(self.head_v1(torch.cat([h_head_v, h_avg_v], 1)))
+        v = torch.tanh(self.head_v2(h_v))
+
+        return {"policy": p, "value": v}
 
 
 class Environment(BaseEnvironment):
