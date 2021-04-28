374b05a934e47ed9223f59d6f7cd74507ece24db
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-04-28 22:40:50 +0900

    add offensive reward

--- handyrl/envs/kaggle/hungry_geese.py
+++ handyrl/envs/kaggle/hungry_geese.py
@@ -298,8 +298,20 @@ class Environment(BaseEnvironment):
 
         return bonus
 
+    def move_to(self, x, a):
+        actions = {
+            "NORTH": (-1, 0),
+            "SOUTH": (1, 0),
+            "WEST": (0, -1),
+            "EAST": (0, 1),
+        }
+        y = ((x[0] + actions[a][0]) % 7, (x[1] + actions[a][1]) % 11)
+        return y
+
     def reward(self):
-        x = self.reward_default()
+        # x = self.reward_default()
+        x = self.reward_offensive()
+        # x = self.reward_defensive()
         return x
 
     def reward_default(self):
@@ -315,50 +327,27 @@ class Environment(BaseEnvironment):
         return rewards
 
     def reward_offensive(self):
-        pass
+        """
+        長さ * 100 + 行動可能なマス数
+        """
+        obs = self.obs_list[-1]
+        geese = obs[0]["observation"]["geese"]
+
+        rewards = {}
+        for p, o in enumerate(obs):
+            length_reward = o["reward"] % 100 * 100
+
+            if o["status"] == "ACTIVE":
+                head = (self.to_row(0, geese[p][0]), self.to_col(0, geese[p][0]))
+                field_reward = self.bfs(self.field, self.move_to(head, o["action"])).sum()
+                rewards[p] = length_reward + field_reward
+            else:
+                rewards[p] = length_reward
+
+        return rewards
 
     def reward_defensive(self):
         pass
-
-    # def reward(self):
-    #     try:
-    #         prev_obs = self.obs_list[-2]
-    #     except IndexError:
-    #         prev_obs = None
-    #     obs = self.obs_list[-1]
-
-    #     alive_rate = {
-    #         0: np.inf,
-    #         2: 3500,
-    #         3: 2500,
-    #         4: 2000,
-    #     }
-    #     num_alive = len([o for o in obs if o["status"] == "ACTIVE"])
-    #     prev_num_alive = len([o for o in prev_obs if o["status"] == "ACTIVE"]) if prev_obs is not None else num_alive
-
-    #     fill = [
-    #         pos
-    #         for goose in obs[0]["observation"]["geese"]
-    #         for pos in goose
-    #     ]
-    #     num_fill = len(fill)
-    #     danger_rate = min(1.0, num_fill ** 2 / alive_rate[num_alive])
-
-    #     death_rate = 200
-    #     death_bonus = (prev_num_alive - num_alive) * death_rate * danger_rate
-
-    #     ht_bonus = self.head_tail_bonus(danger_rate)
-
-    #     rewards = {}
-    #     for p, o in enumerate(obs):
-    #         if o["status"] != "ACTIVE":
-    #             rewards[p] = o["reward"]
-    #         else:
-    #             step_reward = o["reward"] // 100 * 100
-    #             length_reward = o["reward"] % 100 * (1 - danger_rate)
-    #             rewards[p] = step_reward + length_reward + death_bonus + ht_bonus[p]
-
-    #     return rewards
 
     def outcome(self):
         # return terminal outcomes
@@ -469,7 +458,7 @@ class Environment(BaseEnvironment):
             player = 0
 
         b = np.zeros((self.NUM_AGENTS * 4 + 1, 7 * 11), dtype=np.float32)
-        # head = defaultdict(tuple)
+        head = defaultdict(tuple)
         obs = self.obs_list[-1][0]['observation']
 
         for p, geese in enumerate(obs['geese']):
@@ -478,7 +467,7 @@ class Environment(BaseEnvironment):
             # head position
             for pos in geese[:1]:
                 b[0 + pid, pos] = 1
-                # head[pid] = (self.to_row(0, pos), self.to_col(0, pos))
+                head[pid] = (self.to_row(0, pos), self.to_col(0, pos))
             # tip position
             for pos in geese[-1:]:
                 b[4 + pid, pos] = 1
@@ -500,6 +489,7 @@ class Environment(BaseEnvironment):
             b[16, pos] = 1
 
         # movable position
+        self.field = b[8:13].sum(0).reshape(7, 11)
         # b[17] = self.bfs(b[8:13].sum(0).reshape(7, 11), head[0]).reshape(-1)
 
         return b.reshape(-1, 7, 11)
