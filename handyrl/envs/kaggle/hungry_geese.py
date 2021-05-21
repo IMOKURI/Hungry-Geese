abee636914d3c8430d09dd9367f5a49590fd8339
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2021-05-21 21:30:30 +0900

    96 filters

--- handyrl/envs/kaggle/hungry_geese.py
+++ handyrl/envs/kaggle/hungry_geese.py
@@ -209,7 +209,7 @@ class MultiGeeseNet(nn.Module):
 class GeeseNetAlpha(nn.Module):
     def __init__(self):
         super().__init__()
-        layers, filters = 12, 32
+        layers, filters = 12, 96
         self.conv0 = TorusConv2d(17, filters, (3, 3), True)
         self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
 
